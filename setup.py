e7a897fdfa47f3598635f31d7f0550aa6b41a4d9
maze <maze@maze.io>
2025-12-09 11:14:25 +0100

    Version 0.3

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='run',
-    version='0.2',
+    version='0.3',
     description='Python Process Run Control',
     author='Wijnand Modderman',
     author_email='maze@pyth0n.org',
-    url='http://freecode.nl/~maze/python-run/',
+    url='https://github.com/tehmaze/run',
     license='MIT',
     long_description=file('README').read(),
     packages=['run'],
